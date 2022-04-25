f8a2117ec5608abecaa779e3e7939b7aae7f836d
김예리 <dpfl0445@gmail.com>
2022-04-25 23:39:11 +0900

    정보영역 추가

--- dokev-diary/src/components/diary/Diary.jsx
+++ dokev-diary/src/components/diary/Diary.jsx
@@ -5,7 +5,11 @@ import styled from "styled-components";
 const Diary = () => {
     return (
         <DiaryArea>
-            <DiaryBook></DiaryBook>
+            <DiaryBook>
+                <AboutArea>
+                    <About></About>
+                </AboutArea>
+            </DiaryBook>
         </DiaryArea>
     );
 };
@@ -21,8 +25,30 @@ const DiaryBook = styled.div`
     position: absolute;
     background-color: #6ed5fe;
     width: 50%;
-    height: 90vh;
+    height: 95vh;
     border-radius: 15px;
     bottom: -10vh;
+    display: flex;
+    justify-content: center;
+`;
+const AboutArea = styled.div`
+    position: absolute;
+    width: 50%;
+    height: 25%;
+    background-color: white;
+    bottom: 12vh;
+    border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+`;
+const About = styled.div`
+    width: 93%;
+    height: 85%;
+    background-color: white;
+    bottom: 12vh;
+    border-radius: 15px;
+    border: 0.5vh dashed #6ed5fe;
 `;
 export default Diary;
