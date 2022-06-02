aeac26cee1e2fc19d92485c3b7076ad8e5aea9ec
김예리 <dpfl0445@gmail.com>
2022-06-02 23:25:47 +0900

    날짜별 이미지 애니메이션 설정

--- dokev-diary/src/components/diary/Page.jsx
+++ dokev-diary/src/components/diary/Page.jsx
@@ -12,14 +12,20 @@ const Page = () => {
             onClick={() => {
                 setCard(!card);
             }}
+            style={{
+                overflow: "hidden",
+                width: "100%",
+                height: "100%",
+                position: "relative",
+            }}
         >
             <div>
                 {pageData.day[page].src.map((ILN) => {
-                    console.log(ILN);
                     return (
                         <DIMG
                             src={"./img/diaryImg/" + ILN + ".png"}
                             key={ILN}
+                            page={page}
                         />
                     );
                 })}
@@ -31,20 +37,163 @@ const Page = () => {
 };
 const gMove0 = keyframes`
     0%{
-        top:12vh;
+        top:3vh;
     }
     100%{
-        top:15vh;
+        top:7vh;
     }
 `;
+const gMove1_1 = keyframes`
+    0%{
+        right:5vh;
+        top:5vh;
+        
+    }
+    100%{
+        right:12vh;
+        top:-10vh;
+        transform: scale(0.7)
+    }
+`;
+const gMove1_2 = keyframes`
+    0%{
+        left:7vh;
+        top: 0vh;
+    }
+    50%{
+        left:3vh;
+        top:-3vh;
+    }
+    100%{
+        left:-1vh;
+        top:0vh;
+    }
+`;
+
+const gMove2 = keyframes`
+    0%{
+        left:5vh;
+        top:0vh;
+    }
+    10%{
+        left:2vh;
+    }
+    16%{
+        left:4vh;
+        
+    }
+    33%{
+        top:-5vh;
+    }
+    100%{
+        left:5vh;
+        top:-20vh;
+    }
+`;
+const gMove3 = keyframes`
+    0%{
+        transform:rotate(0deg)
+    }
+    33%{
+        transform:rotate(20deg)
+    }
+    66%{
+        transform:rotate(0deg)
+    }
+    100%{
+        transform:rotate(20deg)
+    }
+
+`;
+const gMove4 = keyframes`
+    0%{
+        right:0vh;
+        top:0vh;
+    }
+    20%{
+        right:-2vh;
+        top:-2vh;
+    }
+    40%{
+        right:0vh;
+        top:0vh;
+    }
+    60%{
+        right:-2vh;
+        top:-2vh;
+    }
+    80%{
+        right:0vh;
+        top:0vh;
+    }
+
+    100%{
+        right:-2vh;
+        top:-2vh;
+    }
+`;
+
+const gMove5AD = keyframes`
+    0%{
+        opacity:0;
+    }
+    50%{
+        opacity:0;
+    }
+    100%{
+        opacity:1;
+    }
+`;
+const gMove5H = keyframes`
+    0%{
+        transform:rotate(0deg);
+        top:0;
+        left:-10vh;
+    }
+    90%{
+        transform:rotate(75deg);
+        top:20vh;
+        left: -5vh;
+    }
+    100%{
+        transform:rotate(75deg);
+        top:20vh;
+        left: -5vh;
+    }
+`;
+
 const DIMG = styled.img`
     position: absolute;
-    width: 89.5%;
+    width: 100%;
     height: 50vh;
     cursor: pointer;
     &:last-child {
-        animation: ${(props) => "gMove" + props.key} 1s infinite ease-in-out
-            alternate;
+        animation: ${(props) =>
+                props.page === 0 ? gMove0 : props.page === 1 ? gMove1_2 : null}
+            2s infinite ease-in-out alternate;
+    }
+    &:nth-child(2) {
+        animation: ${(props) =>
+                props.page === 1
+                    ? gMove1_1
+                    : props.page === 2
+                    ? gMove2
+                    : props.page === 3
+                    ? gMove3
+                    : props.page === 4
+                    ? gMove4
+                    : props.page === 5
+                    ? gMove5AD
+                    : null}
+            3s infinite ease-in-out alternate;
+    }
+    &:nth-child(3) {
+        animation: ${(props) => (props.page === 5 ? gMove5AD : null)} 3s
+            infinite ease-in-out alternate;
+    }
+    &:nth-child(4) {
+        animation: ${(props) => (props.page === 5 ? gMove5H : null)} 3s infinite
+            ease-in-out alternate;
     }
 `;
 
